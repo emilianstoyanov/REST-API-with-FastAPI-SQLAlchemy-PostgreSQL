18632c567c6ea7f792556a73861cb26930eb97f1
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-07-23 11:10:38 +0300

    update database

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine=create_engine("postgresql://postgres:stoyanov1010@localhost/item_db",
+                     echo=True)
+
+
+Base=declarative_base()
+
+SessionLocal=sessionmaker(bind=engine)
